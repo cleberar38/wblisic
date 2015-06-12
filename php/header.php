9b9e41a83424556eab83bac3307a01b56f5f893f
cleberar38 <c_leverdo@hotmail.com>
2015-06-12 18:09:46 +0200

    structure

---
+++ php/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html>
+
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, target-densitydpi=medium-dpi, minimal-ui">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="default">
+		<meta name="apple-mobile-web-app-title" content="smap responsive">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		
+		<meta name="keywords" content="webakelive, ishareicare, liveiteria"/>
+		<link rel="canonical" href="http://www.webakelive.com" />
+		<meta name="description" content="Watch we bake your cake live. Live stream 24/7. I share I care to help all. Liveiteria enjoy live streams and our cafeteria at we bake live.">
+		<meta http-equiv="content-language" content="en">
+		<meta http-equiv="content-language" content="sv">
+		<meta property="og:site_name" content="webakelive.com"/>
+		<meta property="og:image" content="../img/logo_wbl_100x100.png" />
+		<meta property="fb:app_id" content="1035319979816679" />
+
+		<title></title>
+
+		<link rel="apple-touch-icon-precomposed" sizes="152x152" href="../img/appicons/apple-touch-icon-152x152.png">
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="../img/appicons/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon-precomposed" sizes="120x120" href="../img/appicons/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="../img/appicons/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon-precomposed" sizes="76x76" href="../img/appicons/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="../img/appicons/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon-precomposed" sizes="57x57" href="../img/appicons/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon-precomposed" href="../img/appicons/apple-touch-icon.png">
+		<link rel="apple-touch-startup-image" href="../img/appicons/Default-Landscape.jpg" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)">
+		<link rel="apple-touch-startup-image" href="../img/appicons/Default-Portrait.jpg" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)">
+		<link rel="apple-touch-startup-image" href="../img/appicons/Default.jpg" media="screen and (max-device-width: 320px)">
+
+		<!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+
+		<!-- inject:css -->
+		
+		<!-- endinject -->
+
+	</head>
+
+<body data-spy="scroll" data-target=".navbar">
+	<!-- This is to keep track and Analysis from GOOGLE -->
+	<?php include_once("ganalytic.php") ?>
